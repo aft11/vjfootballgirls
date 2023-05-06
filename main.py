748d655e61968081a1c1eb3b59aa6f213f0c1c52
aft11 <132625754+aft11@users.noreply.github.com>
2023-05-06 08:48:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods = ["GET", "POST"])
+@app.route('/')
 def index():
   if request.method == "GET":
     return render_template('index.html')
   
+@app.route('/home')
+def home():
   pw = request.form['pw']
   if pw == 'vjsg':
     return render_template('home.html')
