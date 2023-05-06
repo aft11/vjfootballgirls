eed7c90401e305c51164282ecd524f73e0600c03
aft11 <132625754+aft11@users.noreply.github.com>
2023-05-06 08:48:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  if request.method == "GET":
-    return render_template('index.html')
+  return render_template('index.html')
   
 @app.route('/home')
 def home():
